4f8b61c0bd701ebe8614a993670fc355d5f7456e
kyle-park-io <andy3638@naver.com>
2024-10-29 10:51:26 +0900

    refactor: separate goroutine task into package

---
+++ src/goroutinetasks/tasks.go
@@ -0,0 +1,39 @@
+package goroutinetasks
+
+import (
+	"go-distributed-architecture/node"
+	"go-distributed-architecture/send"
+	"go-distributed-architecture/utils"
+
+	"time"
+)
+
+func MsgTask(ch_msg chan send.Msg) {
+	for i := 0; i < 100000; i++ {
+		time.Sleep(3 * time.Second)
+
+		randomMsgType := utils.GetRandom(2)
+		randomMsg := utils.GetRandom(1000)
+		switch randomMsgType {
+		case node.SaveMsg:
+			send.SendMsg(ch_msg, node.SaveMsg, randomMsg)
+		case node.ReturnMsg:
+			send.SendMsg(ch_msg, node.ReturnMsg, 0)
+		}
+	}
+}
+
+func TxTask(ch_tx chan send.Tx) {
+	for i := 0; i < 100000; i++ {
+		time.Sleep(3 * time.Second)
+
+		randomMsgType := utils.GetRandom(2) + 2
+		randomMsg := utils.GetRandom(1000)
+		switch randomMsgType {
+		case node.SaveTx:
+			send.SendTxToRandomNode(ch_tx, node.ADMIN_NODE, node.SaveTx, randomMsg)
+		case node.ReturnTx:
+			send.SendTxToRandomNode(ch_tx, node.ADMIN_NODE, node.ReturnTx, 0)
+		}
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,37 +1,31 @@
 package main
 
 import (
+	"go-distributed-architecture/goroutinetasks"
 	"go-distributed-architecture/node"
 	"go-distributed-architecture/send"
-	"go-distributed-architecture/utils"
 
 	"fmt"
-	"time"
 )
 
 func main() {
 
-	ch_msg := make(chan send.Msg)
+	// ch_msg := make(chan send.Msg)
+	// ch_main_signal := make(chan int)
+	// ch_main := make(chan []send.Msg)
+
+	ch_tx := make(chan send.Tx)
 	ch_main_signal := make(chan int)
-	ch_main := make(chan []send.Msg)
+	ch_main := make(chan []send.Tx)
 
 	for i := 0; i < 3; i++ {
-		go node.RunNode(i, ch_msg, ch_main_signal, ch_main)
+		// go node.RunNode(i, ch_msg, ch_main_signal, ch_main)
+		go node.RunNode2(i, ch_tx, ch_main_signal, ch_main)
 	}
 
 	go func() {
-		for i := 0; i < 100000; i++ {
-			time.Sleep(3 * time.Second)
-
-			randomMsgType := utils.GetRandom(2)
-			randomMsg := utils.GetRandom(1000)
-			switch randomMsgType {
-			case node.SaveMsg:
-				send.SendMsg(ch_msg, node.SaveMsg, randomMsg)
-			case node.ReturnMsg:
-				send.SendMsg(ch_msg, node.ReturnMsg, 0)
-			}
-		}
+		// go goroutinetasks.MsgTask(ch_msg)
+		go goroutinetasks.TxTask(ch_tx)
 	}()
 
 	for {
